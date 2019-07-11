a762013978f9eb7e914a60117485e6f847fde837
Nailya Yafyasova <43023372+NailyaYafyasova@users.noreply.github.com>
2019-07-11 11:23:22 -0400

    added comments to the Game.java

--- experiment1/src/Game.java
+++ experiment1/src/Game.java
@@ -1,21 +1,25 @@
-
 import java.util.Scanner;
 
 public class Game{
+
+	// Two Players of the game
 	private Player playerOne;
 	private Player playerTwo;
 	private Board board;
+	//Game enumeration - starts with 1
 	private int gameNum = 1;
+	// size of the board
 	private int size;
 	
+	// initialization of the game - getting user input
 	public void init() {
-		Scanner scan = new Scanner(System.in);
 		
+		// Getting user input: Players' names and chosen symbols (X/O)
+		Scanner scan = new Scanner(System.in);
 		System.out.print("Please input Player 1's name: ");
 		
 		
 		String name = scan.nextLine();
-		
 		System.out.print("Please input Player 1's symbol: capital X or O");
 		String sym = scan.nextLine();
 		
@@ -29,37 +33,43 @@ public class Game{
 		playerTwo = new Player(name,sym.charAt(0));
 	}
 	
+	
+	// Starting the game, playing the game, and checking for a winner or a draw
 	public void startGame() {
 		
+		// step number in a game
 		int step = 0;
-		
 		Marker marker;
 		
 		System.out.print("Game start.\n");
 		
 		while(true) {
-		
+			// displays the board, asks PlayerOne to make a move
 			System.out.print(board);
-			
 			playerOne.makeMove(board);
 			
 			step ++;
 			
+			// check if the made move makes a win
 			if(board.checkWin() != null || step == board.length * board.length)
 				break;
 			
+			// display the board with a made move
 			System.out.print(board);
 			
+			// asks PlayerTwo to make a move
 			playerTwo.makeMove(board);
 			
 			step ++;
-			
+			// check if the made move makes a win
 			if(board.checkWin() != null || step == board.length * board.length)
 				break;
 		}
 		
+		// if there is a win, save a marker of that player
 		marker = board.checkWin();
 		
+		// check who won the game out of the two players, display game statistics
 		if(playerOne.getMarker().equals(marker)) {
 			System.out.println(board);
 			System.out.println("Player " + playerOne.getName() + " wins!");
@@ -79,16 +89,18 @@ public class Game{
 		}
 	}
 	
+	// running the game - getting board size from user, checking if they would like to play another game
 	public void run() {
 		Scanner scan = new Scanner(System.in);
 		
+		// gets board size
 		while(true) {
-			
+			// if the first game, ask for the board size
 			if (gameNum == 1) {
 				System.out.println("Please input the game size:");
 				size = scan.nextInt();
 			}
-			
+			// if the user doesn't want a new game, finish the game and show statistics
 			else if (gameNum > 1) {
 				System.out.println("Do you want to start a new game? (y/n)");
 				scan.nextLine();
@@ -106,7 +118,7 @@ public class Game{
 					break;
 				}
 			}
-			
+			// start a new game is user says "y"
 			board = new Board(size);
 			startGame();
 			gameNum++;
@@ -114,6 +126,7 @@ public class Game{
 		
 	}
 	
+	// main function that initializes the game and plays it
 	public static void main(String[] args) {
 		Game g = new Game();
 		g.init();
